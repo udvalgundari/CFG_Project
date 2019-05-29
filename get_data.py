faf1073e9eb4d91dd47dae4bf0f67714cc984f16
Carmen Bugarin-Diz <carmen.bugarin.diz@gmail.com>
2019-05-29 14:14:49 +0100

    kslws

--- get_data.py
+++ get_data.py
@@ -48,7 +48,7 @@ auth_query_parameters = {
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("helloworld.html")
     	
 @app.route("/about")
 def about():

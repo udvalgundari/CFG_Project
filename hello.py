a60a937e2686faacf2a31296c7559630cf0ccb97
Udval Gundari <ikhgundari@gmail.com>
2019-05-29 15:07:24 +0100

    adjusting hello.py

--- hello.py
+++ hello.py
@@ -9,13 +9,3 @@ def hello():
 @app.route("/<name>")
 def hi(name):
    return render_template("helloworld.html", userName=name)
-
-
-
-curl -s --user 'api:YOUR_API_KEY' \
-    https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages \
-    -F from='Excited User <mailgun@YOUR_DOMAIN_NAME>' \
-    -F to=YOU@YOUR_DOMAIN_NAME \
-    -F to=bar@example.com \
-    -F subject='Hello' \
-    -F text='Testing some Mailgun awesomeness!'
